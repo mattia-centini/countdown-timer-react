717c272fac35ab46b30a6b0152b89b501ece537f
Mattia Centini <mattia.centini@gmail.com>
2020-09-28 12:39:04 +0200

    created components

--- src/Timer.js
+++ src/Timer.js
@@ -10,7 +10,24 @@ function Timer({ timeLeft, setTimeLeft, calculateTimeLeft }) {
     return () => clearTimeout(timer);
   });
 
-  return <div></div>;
+  //using this variable to push a new timeLeft component
+  const timerComponents = [];
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? timerComponents : <span>Time's Up!</span>}
+    </div>
+  );
 }
 
 export default Timer;
